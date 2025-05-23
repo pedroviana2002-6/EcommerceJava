da54f893a083c43f4c06e4dc0aef5747a935da51
Pedro Viana <78880334+pedroviana2002-6@users.noreply.github.com>
2025-05-22 21:24:19 -0300

    Update Fornecedor.java

--- src/br/sistema/conta/Fornecedor.java
+++ src/br/sistema/conta/Fornecedor.java
@@ -3,10 +3,12 @@ package br.sistema.conta;
 public class Fornecedor {
     private String nome;
     private String cnpj;
+    private String endereco; 
 
-    public Fornecedor(String nome, String cnpj) {
+	public Fornecedor(String nome, String cnpj, String endereco) {
         this.nome = nome;
         this.cnpj = cnpj;
+        this.endereco = endereco;
     }
 
 	public String getNome() {
@@ -23,5 +25,13 @@ public class Fornecedor {
 
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
+	}
+	
+    public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
 	}
 }
